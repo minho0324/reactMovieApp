03d7e4c598b34c0152994a986de479805faacdc2
minhoHan <minho117811@gamil.com>
2022-11-01 23:11:27 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,39 @@
-import { useState, useEffect } from "react";
-
-
-function Hello() {
-
-  useEffect(() => {
-    console.log('hi1 :)');
-    return () => console.log('bye1 :(');
-  }, []);
-
-  useEffect(function() {
-    console.log('hi2 :)');
-    return function() {
-      console.log('bye2 :(');
-    }
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null }
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-  }
-  
-  export default App;
-  
+}
 
-
-
-
-  
-      // const [counter, setValue] = useState(0);
-      // const [keyword, setKeyword] = useState("");
-      // const onClick = () => setValue((prev) => prev + 1);
-      // const onChange = (event) => setKeyword(event.target.value);
-      // useEffect(() => {               //useEffect()는 한번만 실행하고 싶은 함수를 실행한다.
-      //   console.log('I run only once');
-      // }, []);
-      
-      // useEffect(() => {
-      //   console.log("I run when 'keyword' changes.");
-      // }, [keyword]);                  //value값인 keyword가 변화할때마다 작동한다.
-    
-    
-      // useEffect(() => {
-      //   console.log("I run when 'counter' changes.");
-      // }, [counter]);      
-      
-      
-      // useEffect(() => {
-      //   console.log("I run when 'keyword & counter' changes.");
-      // }, [keyword, counter]);         //keyword, counter 두가지 모두 변화가 있을때마다 작동한다.
-
+export default App;
