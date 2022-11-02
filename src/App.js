7b8c10c9bc11948e4b0ca8c7014208ecfe02f13d
minho0324 <minho117811@gmail.com>
2022-11-02 22:15:38 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  } from "react-router-dom";
+import Home from './routes/Home';
+import Detail from './components/Detail';
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={ <Home />} />
+        <Route path="/movie" element={ <Detail />} /> {/* element 안의 jsx를 렌더해서 보여줌 */}
+        {/* <Route path="/hi" element={ <h1>hi</h1>} /> http://localhost:3001/hi => 들어가면 hi를 렌더한다 */} 
+      </Route>
+    )
+  );
+  
+  function App() {
+    return <RouterProvider router={router} />;
+  }
 
-
-function App() {
-  return null;
-}
 
 export default App;
