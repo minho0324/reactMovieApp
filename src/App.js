9f3f45172019202d7c2dd9feebca984136fbe325
minho0324 <minho117811@gmail.com>
2022-11-03 22:13:23 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Detail from './routes/Detail';
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route basename={process.env.PUBLIC_URL} path="/" element={ <Home />} />
-        <Route basename={process.env.PUBLIC_URL} path="/movie" element={ <Detail />} /> {/* element 안의 jsx를 렌더해서 보여줌 */}
+        <Route  path="/" element={ <Home />} />
+        <Route  path="/movie" element={ <Detail />} /> {/* element 안의 jsx를 렌더해서 보여줌 */}
         {/* <Route path="/hi" element={ <h1>hi</h1>} /> http://localhost:3001/hi => 들어가면 hi를 렌더한다 */} 
       </Route>
     )
